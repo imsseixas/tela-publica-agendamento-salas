ed19ae8c2cedbee166881cc8982f1603e2d9bcde
Italo Matheus <imsseixas@gmail.com>
2025-08-17 20:35:16 -0300

    Update salas seg 17:08

--- src/app/page.js
+++ src/app/page.js
@@ -28,13 +28,25 @@ const useMediaQuery = (query) => {
 
 // DADOS DE EXEMPLO
 const agendamentos = [
-  { sala: 'Auditório Jorge Filgueira', inicio: '08:00', fim: '16:30', nome: 'Saúde Mental', cor: 'bg-blue-500', responsavel: 'Prof. João' },
-  { sala: 'Sala de treinamento 01', inicio: '10:00', fim: '11:00', nome: 'Reunião Interna', cor: 'bg-pink-500', responsavel: 'Coordenação' },
-  { sala: 'Sala de treinamento 03', inicio: '13:30', fim: '15:00', nome: 'Oficina Maker', cor: 'bg-purple-500', responsavel: 'Prof. Marina' },
-  { sala: 'Salão Nobre', inicio: '14:30', fim: '16:00', nome: 'Workshop de Programação', cor: 'bg-yellow-500', responsavel: 'Prof. Carlos' },
-  { sala: 'Sala de treinamento 04', inicio: '09:00', fim: '10:30', nome: 'Seminário X', cor: 'bg-green-500', responsavel: 'Prof. Ana' },
-  { sala: 'SENEP', inicio: '11:00', fim: '16:30', nome: 'Palestra Y', cor: 'bg-red-500', responsavel: 'Prof. Lucas' },
-  { sala: 'Auditório Jorge Filgueira', inicio: '16:00', fim: '17:30', nome: 'Palestra sobre Sustentabilidade', cor: 'bg-orange-500', responsavel: 'Prof. Fernanda' }
+  { sala: 'Sala de treinamento 01', inicio: '07:30', fim: '08:30', nome: 'SERVIÇO DE DERMATOLOGIA [In-person]', cor: 'bg-blue-400', responsavel: '' },
+  { sala: 'Sala de treinamento 01', inicio: '10:00', fim: '12:00', nome: 'DISCUSSÃO DE CASOS', cor: 'bg-blue-400', responsavel: '' },
+  { sala: 'Sala de treinamento 01', inicio: '13:00', fim: '15:00', nome: 'PROGRAMA DE RESIÊNCIA MULTIPROFISSIONAL EM SAÚDE DA UNEM', cor: 'bg-blue-400', responsavel: 'Sra. Ângela Cristina F. Goes'},
+  { sala: 'Sala de treinamento 01', inicio: '15:00', fim: '16:30', nome: 'Treinamento : Elaboração de agenda [In-person]', cor: 'bg-blue-400', responsavel: 'Alane Fidelis de Andrade' },
+  { sala: 'Sala de treinamento 01', inicio: '18:00', fim: '22:00', nome: 'AULA FACULDADE SANTA CASA', cor: 'bg-blue-400', responsavel: '' },
+  { sala: 'Sala de treinamento 02', inicio: '08:00', fim: '11:00', nome: 'Camila Ferreira Houat', cor: 'bg-pink-400', responsavel: 'Camila Ferreira Houat' },
+  { sala: 'Sala de treinamento 02', inicio: '14:30', fim: '16:30', nome: 'Treinamento Lactário e SENEP', cor: 'bg-pink-400', responsavel: 'Brenna Loreny da Rocha Carvalho' },
+  { sala: 'Sala de treinamento 02', inicio: '18:00', fim: '22:00', nome: 'AULA FACULDADE SANTA CASA', cor: 'bg-pink-400', responsavel: '' },
+  { sala: 'Sala de treinamento 03', inicio: '06:30', fim: '09:00', nome: 'SERVIÇO DE ORTOPEDIA [In-person]', cor: 'bg-green-400', responsavel: '' },
+  { sala: 'Sala de treinamento 03', inicio: '09:00', fim: '11:00', nome: 'SESSÃO DE CLINCA MÉDICA_INTERNOS', cor: 'bg-green-400', responsavel: 'Dr. Mateus' },
+  { sala: 'Sala de treinamento 03', inicio: '14:00', fim: '16:00', nome: 'SESSÃO MEDICINA INTENSIVA/AULA CLINICA RESIDÊNCIA', cor: 'bg-green-400', responsavel: 'Dr. Reinaldo' },
+  { sala: 'Sala de treinamento 03', inicio: '18:00', fim: '22:00', nome: 'AULA FACULDADE SANTA CASA', cor: 'bg-green-400', responsavel: '' },
+  { sala: 'Sala de treinamento 04', inicio: '06:30', fim: '07:30', nome: 'SERVIÇO DE ANESTESTESIOLOGIA', cor: 'bg-gray-400', responsavel: '' },
+  { sala: 'Sala de treinamento 04', inicio: '08:00', fim: '11:00', nome: 'Camila Ferreira Houat', cor: 'bg-gray-400', responsavel: 'Camila Ferreira Houat' },
+  { sala: 'Sala de treinamento 04', inicio: '14:00', fim: '17:00', nome: 'Camila Ferreira Houat', cor: 'bg-gray-400', responsavel: 'Camila Ferreira Houat' },
+  { sala: 'Sala de treinamento 04', inicio: '18:00', fim: '22:00', nome: 'AULA FACULDADE SANTA CASA', cor: 'bg-gray-400', responsavel: '' },
+  { sala: 'SENEP', inicio: '07:00', fim: '10:00', nome: 'SERVIÇO DE CIRURGIA GERAL [In-person]', cor: 'bg-cyan-400', responsavel: '' },
+  { sala: 'SENEP', inicio: '10:00', fim: '11:00', nome: 'Discussão de Casos Clínicos', cor: 'bg-cyan-400', responsavel: 'Gilson Filho' },
+  { sala: 'SENEP', inicio: '14:00', fim: '17:00', nome: 'Luciana Magnavita Rocha', cor: 'bg-cyan-400', responsavel: 'Luciana Magnavita Rocha' },
 ];
 
 export default function Home() {
@@ -58,7 +70,7 @@ export default function Home() {
     'Auditório Jorge Filgueira'
   ];
   const horarios = [];
-  for (let h = 7; h <= 23; h++) {
+  for (let h = 6; h <= 23; h++) {
     horarios.push(`${h.toString().padStart(2, '0')}:00`);
     if (interval === 30 && h < 23) {
       horarios.push(`${h.toString().padStart(2, '0')}:30`);
@@ -164,7 +176,7 @@ export default function Home() {
                           }}
                         >
                           <div className="text-sm font-semibold truncate leading-tight">{ag.nome}</div>
-                          <div className="text-xs text-white/90 italic text-right">{ag.responsavel || 'Prof. Desconhecido'}</div>
+                          <div className="text-xs text-white/90 italic text-right">{ag.responsavel || 'Instrutor não informado'}</div>
                         </div>
                       );
                     })}
